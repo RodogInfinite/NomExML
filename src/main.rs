f9c0e48293b002886f06c2d93163826406fd5e47
RodogInfinite <wjroland@gmail.com>
2023-04-23 18:37:15 -0400

    "Refactor: Consolidate lifetimes and move functions to enum impl blocks"

--- src/main.rs
+++ src/main.rs
@@ -15,35 +15,61 @@ enum Namespace<'ns> {
 }
 
 #[derive(Clone, Debug, PartialEq)]
-enum Tag<'tag, 'ns> {
-    Open(&'tag str),
-    Close(&'tag str),
-    NS(Namespace<'ns>, Box<Tag<'tag, 'ns>>),  // NS(Prefix, Tag::Open | Tag::Close)
+
+enum Tag<'a> {
+    Open(&'a str),
+    Close(&'a str),
+    NS(Namespace<'a>, Box<Tag<'a>>),  // NS(Prefix, Tag::Open | Tag::Close)
 }
 
-#[derive(Clone, Debug, PartialEq)]
-enum Element<'tag, 'ns, 'elem> {
-    Node(
-        Tag<'tag, 'ns>,
-        Box<Element<'tag, 'ns, 'elem>>,
-        Tag<'tag, 'ns>,
-    ),
-    Content(&'elem str),
-    Nested(Vec<Element<'tag, 'ns, 'elem>>),
-}
 
-// A helper function to create a namespaced tag.
-fn create_ns_tag<'a>(prefix: Option<&'a str>, local_name: &'a str, tag_type: fn(&'a str) -> Tag<'a, 'a>) -> Tag<'a, 'a> {
+fn create_ns_tag<'a>(prefix: Option<&'a str>, local_name: &'a str, tag_type: fn(&'a str) -> Tag<'a>) -> Tag<'a> {
     match prefix {
-        // If a prefix is present, create a Tag::NS with the Namespace::Prefix and the tag created by calling the tag_type function.
         Some(p) => Tag::NS(Namespace::Prefix(p), Box::new(tag_type(local_name))),
-        // If no prefix is present, create the tag directly by calling the tag_type function.
         None => tag_type(local_name),
     }
 }
 
+
+impl<'a> Tag<'a> {
+    fn parse(input: &'a str) -> IResult<&'a str, Self> {
+        alt((
+            map(
+                delimited(
+                    tag("<"),
+                    tuple((
+                        opt(preceded(alpha1, tag(":"))),
+                        take_while1(|c: char| c.is_alphanumeric() || c == '_'),
+                    )),
+                    tag(">"),
+                ),
+                |(prefix, local_name)| create_ns_tag(prefix, local_name, Tag::Open),
+            ),
+            map(
+                delimited(
+                    tag("</"),
+                    tuple((
+                        opt(preceded(alpha1, tag(":"))),
+                        take_while1(|c: char| c.is_alphanumeric() || c == '_'),
+                    )),
+                    tag(">"),
+                ),
+                |(prefix, local_name)| create_ns_tag(prefix, local_name, Tag::Close),
+            ),
+        ))(input)
+    }
+}
+
+
+#[derive(Clone, Debug, PartialEq)]
+enum Element<'a> {
+    Node(Tag<'a>, Box<Element<'a>>, Tag<'a>),
+    Content(&'a str),
+    Nested(Vec<Element<'a>>),
+}
+
 // The parse_tag function is responsible for parsing XML tags (opening and closing).
-fn parse_tag<'a>(input: &'a str) -> IResult<&'a str, Tag<'a, 'a>> {
+fn parse_tag<'a>(input: &'a str) -> IResult<&'a str, Tag<'a>> {
     alt((
         // Parse opening tags
         map(
@@ -76,64 +102,47 @@ fn parse_tag<'a>(input: &'a str) -> IResult<&'a str, Tag<'a, 'a>> {
     ))(input)
 }
 
-fn parse_content(input: &str) -> IResult<&str, &str> {
-    take_until("</")(input)
-}
+impl<'a> Element<'a> {
+    fn parse_content(input: &'a str) -> IResult<&'a str, &'a str> {
+        take_until("</")(input)
+    }
 
-fn parse_recursive(input: &str) -> IResult<&str, Element, nom::error::Error<&str>> {
-    // Parse the opening tag of the element
-    let (input, open_tag) = parse_tag(input)?;
-    
-    // Recursively parse the children of the current element using many0 combinator
-    // many0 applies the parse_recursive function zero or more times until it fails
-    let (input, children) = many0(parse_recursive)(input)?;
-    
-    // Parse the content of the current element (text between tags)
-    let (input, content) = parse_content(input)?;
+    fn parse_recursive(input: &'a str) -> IResult<&'a str, Self> {
+        let (input, open_tag) = Tag::parse(input)?;
+        let (input, children) = many0(Self::parse_recursive)(input)?;
+        let (input, content) = Self::parse_content(input)?;
+        let (input, close_tag) = Tag::parse(input)?;
 
-    // Parse the closing tag of the element
-    let (input, close_tag) = parse_tag(input)?;
+        match (&open_tag, &close_tag) {
+            (Tag::Open(open_name), Tag::Close(close_name))
+            | (Tag::NS(Namespace::Prefix(open_name), _), Tag::NS(Namespace::Prefix(close_name), _))
+                if open_name == close_name =>
+            {
+                let child_element = if !content.is_empty() {
+                    Element::Content(content)
+                } else if children.len() == 1 {
+                    children.into_iter().next().unwrap()
+                } else {
+                    Element::Nested(children)
+                };
 
-    // Match the opening and closing tags to ensure they are of the same type (Open and Close or NS with same prefixes)
-    let node = match (&open_tag, &close_tag) {
-        // Check if the opening and closing tags are either both Open/Close or both NS with the same prefix
-        (Tag::Open(open_name), Tag::Close(close_name))
-        | (Tag::NS(Namespace::Prefix(open_name), _), Tag::NS(Namespace::Prefix(close_name), _))
-            if open_name == close_name =>
-        {
-            // Determine the child element depending on the content and the number of children
-            let child_element = if !content.is_empty() {
-                // If content is not empty, create an Element::Content variant
-                Element::Content(content)
-            } else if children.len() == 1 {
-                // If there is only one child, use that child as the nested element
-                children.into_iter().next().unwrap()
-            } else {
-                // Otherwise, create an Element::Nested variant with all the children
-                Element::Nested(children)
-            };
-
-            // Return a new Element::Node variant with the opening tag, the child element, and the closing tag
-            Ok((
+                Ok((
+                    input,
+                    Element::Node(open_tag, Box::new(child_element), close_tag),
+                ))
+            }
+            _ => Err(nom::Err::Error(nom::error::Error::new(
                 input,
-                Element::Node(open_tag, Box::new(child_element), close_tag),
-            ))
+                nom::error::ErrorKind::Verify,
+            ))),
         }
-        // If the opening and closing tags don't match or are invalid, return an error with the Verify ErrorKind
-        _ => Err(nom::Err::Error(nom::error::Error::new(
-            input,
-            nom::error::ErrorKind::Verify,
-        ))),
-    };
-
-    // Return the resulting node
-    node
+    }
 }
 
 
 fn main() {
     let input = "<root><inner_tag1>inner_tag1 content</inner_tag1><inner_tag2>2</inner_tag2><tst:inner_tag3>3</tst:inner_tag3><tst:inner_tag4><inner_inner_tag1>inner_inner_tag1 content</inner_inner_tag1><header>header contents></header><inner_inner_tag1>inner_inner_tag1 content2</inner_inner_tag1></tst:inner_tag4></root>";
-    let (tail, result) = parse_recursive(input).unwrap();
+    let (tail, result) = Element::parse_recursive(input).unwrap();
     println!("result: {:#?}", result);
     println!("tail: {:?}", tail);
 }
