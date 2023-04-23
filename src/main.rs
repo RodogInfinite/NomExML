a3b717a36b26b7d66819e063aa9c1e7e3f737f1f
RodogInfinite <wjroland@gmail.com>
2023-04-22 21:16:58 -0400

    Saving progress (Doesn't work)

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,61 @@
 use nom::{
-    bytes::complete::{tag, take_until, take_till},
-    character::complete::alphanumeric1,
+    bytes::complete::{tag, take_until, take_till, take_while1},
+    character::complete::{alphanumeric1, alpha1},
     multi::many0,
     sequence::{delimited, tuple, preceded, pair},
-    IResult, branch::alt, combinator::{map},
+    IResult, branch::alt, combinator::{map, opt},
 };
 
-#[derive(Debug)]
+#[derive(Clone,Debug,PartialEq)]
 enum Namespace<'a> {
     Prefix(&'a str),
     LocalName(&'a str),
     URI(&'a str),
 }
 
-#[derive(Debug)]
-enum Element<'a> {
-    OpenTag(&'a str),
-    CloseTag(&'a str),
-    Node(&'a str, Vec<Element<'a>>, &'a str),
-    Text(&'a str),
+#[derive(Clone,Debug,PartialEq)]
+enum Tag<'a> {
+    Open(&'a str),
+    Close(&'a str),
     NS(Namespace<'a>)
 }
+
+#[derive(Debug,PartialEq)]
+enum Element<'a> {
+    Node(Tag<'a>, Vec<Element<'a>>, Tag<'a>),
+    Content(&'a str),
+    Tag(Tag<'a>)
+}
+
 
 fn parse_processing_instructions(input: &str) -> IResult<&str, &str> {
     delimited(tag("<?"), take_until("?>"), tag("?>"))(input)
 }
 
-fn parse_tag(input: &str) -> IResult<&str, Element> {
+fn parse_tag(input: &str) -> IResult<&str, Tag> {
     alt((
         map(
             delimited(
-                alt((tag("</"), tag("<"))),
+                tag("<"),
                 pair(
-                    alphanumeric1,
-                    preceded(
+                    alpha1,
+                    opt(preceded(
                         tag(":"),
-                        take_till(|c| c == '>' || c == '<'),
-                    ),
+                        take_while1(|c: char| c.is_alphanumeric() || c == '_'),
+                    )),
                 ),
                 tag(">"),
             ),
-            |(prefix, tag_name)| {
-                Element::NS(Namespace::Prefix(prefix))
+            |(prefix, opt_tag_name)| match opt_tag_name {
+                Some(tag_name) => Tag::NS(Namespace::Prefix(prefix)),
+                None => Tag::Open(prefix),
             },
         ),
         map(delimited(tag("</"), take_until(">"), tag(">")), |tag_name| {
-            Element::CloseTag(tag_name)
+            Tag::Close(tag_name)
         }),
         map(delimited(tag("<"), take_until(">"), tag(">")), |tag_name| {
-            Element::OpenTag(tag_name)
+            Tag::Open(tag_name)
         }),
     ))(input)
 }
@@ -59,39 +66,43 @@ fn parse_content(input: &str) -> IResult<&str, &str> {
 
 fn parse_recursive(input: &str) -> IResult<&str, Vec<Element>> {
     many0(alt((
-        parse_tag,
+        map(parse_tag, |tag| {
+            Element::Node(tag.clone(), vec![], tag)
+        }),
         map(tuple((parse_tag, parse_recursive, parse_tag)), |(open_tag, nodes, close_tag)| {
             match (open_tag, close_tag) {
-                (Element::OpenTag(open), Element::CloseTag(close)) => Element::Node(open, nodes, close),
+                (Tag::Open(open), Tag::Close(close)) => {
+                    Element::Node(Tag::Open(open), nodes, Tag::Close(close))
+                }
                 _ => unreachable!(),
             }
         }),
         map(parse_content, |content| {
-            Element::Text(content)
+            Element::Content(content)
         }),
     )))(input)
 }
 
-fn parse(input: &str) -> IResult<&str, Element> {
-    let (tail, mut result) = parse_recursive(input)?;
 
-    let tag_element = match result.remove(0) {
-        Element::OpenTag(tag) => tag,
-        _ => unreachable!(),
-    };
-
-    Ok((tail, Element::Node(tag_element, result, "")))
-}
 
 fn main() {
     let input = "<root><inner_tag1>inner_tag1 content</inner_tag1><inner_tag2>2</inner_tag2><tst:inner_tag3>3</tst:inner_tag3></root>";
-    let (tail, result) = parse(input).unwrap();
+    let (tail, result) = parse_recursive(input).unwrap();
     println!("result: {:?}", result);
     println!("tail: {:?}", tail);
-    if let Element::Node(tag, children, _) = result {
-        println!("Tag element: {:?}", tag);
-        for res in children {
-            println!("{:?}", res);
-        }
-    }
+
+    // assert_eq!(result,Element::Node(Tag::Open("root"), vec![
+    //     Element::Node(Tag::Open("inner_tag1"), vec![
+    //         Element::Content("inner_tag1 content"), 
+    //     ], Tag::Close("inner_tag1")),
+    //     Element::Node(Tag::Open("inner_tag2"), vec![
+    //         Element::Content("2"), 
+    //     ], Tag::Close("inner_tag2")),
+    //     Element::Node(Tag::Open("inner_tag3"), vec![
+    //         Element::NS(Namespace::Prefix("tst")),
+    //         Element::Content("3"), 
+    //         Element::NS(Namespace::Prefix("tst"))
+    //     ], Tag::Close("inner_tag3")),
+    //     ], Tag::Close("root"))
+    // )
 }
