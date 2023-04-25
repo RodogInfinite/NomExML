0cf956b7f16154ca22f8d364c07a4e6554f90f46
RodogInfinite <wjroland@gmail.com>
2023-04-24 22:12:57 -0400

    Format

--- tests/nested.rs
+++ tests/nested.rs
@@ -3,7 +3,7 @@ use nomexml::{Element, Namespace};
 #[test]
 fn test_nested_recursion() {
     let input = "<root><inner_tag1>inner_tag1 content</inner_tag1><inner_tag2>2</inner_tag2><tst:inner_tag3>3</tst:inner_tag3><tst:inner_tag4><inner_inner_tag1>inner_inner_tag1 content</inner_inner_tag1><header>header contents</header><inner_inner_tag1>inner_inner_tag1 content2</inner_inner_tag1><inner_inner_tag2><inner_inner_inner_tag1>inner_inner_inner_tag1 content</inner_inner_inner_tag1></inner_inner_tag2></tst:inner_tag4></root>";
-    
+
     let (_, result) = Element::parse_xml_str(input).unwrap();
 
     assert_eq!(
