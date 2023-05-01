b3482308a2ee95f121c54bf6cc52715b7f15d5ec
RodogInfinite <wjroland@gmail.com>
2023-05-01 17:15:25 -0400

    Added first compliance test

--- tests/compliance.rs
+++ tests/compliance.rs
@@ -1,9 +1,59 @@
-// [#test]
-// fn x() {
+use std::{fs::File, error::Error};
 
-// <!DOCTYPE doc [
-// <!ELEMENT doc (#PCDATA)>
-// ]>
-// <doc></doc>
+use nomexml::{parse_file, Document, Tag, TagState,ConditionalState,{declaration::{Declaration,DeclarationContent,Mixed}}};
 
-// }
+fn test_xml_file<'a>(file_number: &str) -> Result<Document<'a>, Box<dyn Error>>{
+
+    let mut file = File::open(format!("tests/xmltest/valid/sa/{file_number}.xml"))?;
+    let document = parse_file(&mut file)?;
+    Ok(document)
+}
+
+
+#[test]
+fn test_valid_sa_001() -> Result<(), Box<dyn Error>> {
+    let document = test_xml_file("001")?;
+    assert_eq!(
+        document,
+        Document::Nested(vec![
+            Document::Declaration(Some(Declaration::DocType {
+                name: Some("doc".into()),
+                external_id: None,
+                int_subset: Some(vec![
+                    Declaration::Element {
+                        name: Some("doc".into()),
+                        content_spec: Some(DeclarationContent::Spec {
+                            mixed: Mixed::PCDATA {
+                                names: None,
+                                parsed: true,
+                                conditional_state: ConditionalState::None,
+                            },
+                            children: None,
+                        }),
+                    },
+                ]),
+            })),
+            Document::Element(
+                Tag::Tag {
+                    name: "doc".into(),
+                    namespace: None,
+                    state: TagState::Start,
+                },
+                Box::new(Document::Empty),
+                Tag::Tag {
+                    name: "doc".into(),
+                    namespace: None,
+                    state: TagState::End,
+                },
+            ),
+        ]),
+    );
+    Ok(())
+}
+
+
+#[test]
+fn test_valid_sa_002() {
+    let document = test_xml_file("002");
+
+}
