de767ea1646782f87af06320aa3eea6ec71bc0e5
RodogInfinite <wjroland@gmail.com>
2023-05-01 17:23:53 -0400

    clear main for future development

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
-use std::error::Error;
+use std::{error::Error, fs::File};
 
-use nomexml::{parse_file, Document};
+use nomexml::{parse_file, read_file, Document};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let input = "<!DOCTYPE doc [
-        <!ELEMENT doc (#PCDATA)>
-        ]>
-        <doc ></doc>
-        ";
-
-    let (_, res) = Document::parse_xml_str(input)?;
-    println!("{res:?}");
-
     Ok(())
 }
