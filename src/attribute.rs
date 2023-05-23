5118eeff03ce4e1233df03fc2c020b86cf976ee8
RodogInfinite <wjroland@gmail.com>
2023-05-22 22:17:44 -0400

    pass test_valid_sa_006

--- src/attribute.rs
+++ src/attribute.rs
@@ -28,15 +28,14 @@ pub enum Attribute<'a> {
     },
     Required,
     Implied,
-
 }
 
 impl<'a> Attribute<'a> {
     fn parse_literal(input: &'a str) -> IResult<&'a str, &'a str> {
         delimited(
-            tag("\""),
-            take_while(|c: char| c != '\"' && c != '<' && c != '&'),
-            tag("\""),
+            alt((tag("'"), tag("\""))),
+            take_while(|c: char| c != '\'' && c != '\"' && c != '<' && c != '&'),
+            alt((tag("'"), tag("\""))),
         )(input)
     }
 
@@ -56,7 +55,7 @@ impl<'a> Attribute<'a> {
 
     pub fn parse_attribute_instance(input: &'a str) -> IResult<&'a str, Attribute<'a>> {
         let (input, name) = take_while1(|c: char| c.is_alphanumeric() || c == '_')(input)?;
-        let (input, _) = Document::parse_with_whitespace(input,tag("="))?;
+        let (input, _) = Document::parse_with_whitespace(input, tag("="))?;
         let (input, value) = Self::parse_literal(input)?;
         Ok((
             input,
