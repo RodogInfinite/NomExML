3d40c96416fb4c47665d7caffe438b1756eeb75d
RodogInfinite <wjroland@gmail.com>
2024-02-04 11:42:13 -0500

    Add `test_valid_not_sa_002`

--- tests/valid_not_sa_compliance.rs
+++ tests/valid_not_sa_compliance.rs
@@ -73,3 +73,51 @@ fn test_valid_not_sa_001() -> Result<(), Box<dyn Error>> {
     );
     Ok(())
 }
+
+#[test]
+fn test_valid_not_sa_002() -> Result<(), Box<dyn Error>> {
+    let document = test_valid_ext_sa_file(
+        "002",
+        Config {
+            external_parse_config: ExternalEntityParseConfig {
+                allow_ext_parse: true,
+                ignore_ext_parse_warning: true,
+                base_directory: Some("tests/xmltest/valid/not-sa".into()),
+            },
+        },
+    )?;
+
+    assert_eq!(
+        document,
+        Document::Nested(vec![
+            Document::Prolog {
+                xml_decl: None,
+                misc: None,
+                doc_type: Some(DocType {
+                    name: Name::new(None, "doc"),
+                    external_id: Some(ExternalID::System("002.ent".to_string())),
+                    int_subset: Some(vec![InternalSubset::MarkupDecl(
+                        MarkupDeclaration::Element {
+                            name: Name::new(None, "doc"),
+                            content_spec: Some(DeclarationContent::Empty),
+                        }
+                    ),]),
+                }),
+            },
+            Document::Element(
+                Tag {
+                    name: Name::new(None, "doc"),
+                    attributes: None,
+                    state: TagState::Start,
+                },
+                Box::new(Document::Empty),
+                Tag {
+                    name: Name::new(None, "doc"),
+                    attributes: None,
+                    state: TagState::End,
+                },
+            ),
+        ]),
+    );
+    Ok(())
+}
