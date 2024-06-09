6234e2279ba80419882da878c59728ed18d46816
RodogInfinite <wjroland@gmail.com>
2024-06-09 18:23:08 -0400

    add error handling

--- examples/extract_information_manual.rs
+++ examples/extract_information_manual.rs
@@ -10,76 +10,18 @@ use nom_xml::{
 #[derive(Debug, Default)]
 struct Book {
     isbn: String,
-    author: Author,
+    authored_by: AuthoredBy,
     title: String,
     genre: String,
     series_number: u8,
     description: String,
 }
-
-#[derive(Debug, Default, Clone)]
-struct Author {
-    pen_name: String,
-    authors: Vec<AuthorName>,
-}
-#[derive(Debug, Default, Clone)]
-struct AuthorName {
-    first_name: String,
-    last_name: String,
-}
-impl UpdateField for AuthorName {
-    fn update_field(&mut self, tag: &Tag, doc: &Document) {
-        let field_name = &tag.name.local_part;
-        if let Document::Content(Some(value)) = &doc {
-            match field_name.as_str() {
-                "first_name" => {
-                    self.first_name = value.to_string();
-                }
-                "last_name" => {
-                    self.last_name = value.to_string();
-                }
-                e => {
-                    eprintln!("Unknown field in AuthorName: {}", e);
-                }
-            }
-        } else {
-            eprintln!("Content is missing in AuthorName");
-        }
-    }
-}
-impl UpdateField for Author {
-    fn update_field(&mut self, tag: &Tag, doc: &Document) {
-        match (tag.name.local_part.as_str(), doc) {
-            ("pen_name", Document::Content(Some(value))) => {
-                self.pen_name = value.to_string();
-            }
-            ("authors", Document::Nested(elements)) => {
-                elements.iter().for_each(|record| {
-                    if let Document::Element(_, inner_doc, _) = record {
-                        let mut author_name = AuthorName::default();
-                        if let Document::Nested(inner_elements) = inner_doc.as_ref() {
-                            inner_elements.iter().for_each(|inner_record| {
-                                if let Document::Element(tag, content, _) = inner_record {
-                                    author_name.update_field(tag, content);
-                                }
-                            });
-                            self.authors.push(author_name);
-                        } else {
-                            eprintln!("Content is missing in Author authors");
-                        }
-                    }
-                });
-            }
-            _ => eprintln!("Unknown field in Author: {}", tag.name.local_part),
-        }
-    }
-}
-
 impl UpdateField for Book {
-    fn update_field(&mut self, tag: &Tag, doc: &Document) {
+    fn update_field(&mut self, tag: &Tag, doc: &Document) -> Result<()> {
         let field_name = &tag.name.local_part;
+
         if let Some(attributes_vec) = &tag.attributes {
-            attributes_vec.iter().for_each(|attr| {
+            for attr in attributes_vec.iter() {
                 if let Attribute::Instance {
                     name,
                     value: AttributeValue::Value(attr_val),
@@ -89,21 +31,22 @@ impl UpdateField for Book {
                         self.isbn = attr_val.to_string();
                     }
                 }
-            });
+            }
         }
+
         match &doc {
             Document::Nested(_) => {
-                doc.iter_with_depth(1).for_each(|record| {
-                    if let Document::Element(ref tag, ref inner_doc, _) = record {
+                for element in doc.iter_with_depth(1) {
+                    if let Document::Element(ref tag, ref inner_doc, _) = element {
                         if "authored_by" == tag.name.local_part {
-                            update_fields(inner_doc, &mut self.author);
+                            update_fields(inner_doc, &mut self.authored_by)?;
                         } else {
-                            self.update_field(tag, inner_doc);
+                            self.update_field(tag, inner_doc)?;
                         }
                     } else {
-                        eprintln!("Unknown field: {record:#?}");
+                        return Err(format!("Unknown field: {element:#?}").into());
                     }
-                });
+                }
             }
 
             Document::Content(Some(value)) => match field_name.as_str() {
@@ -120,31 +63,107 @@ impl UpdateField for Book {
                     self.description = value.to_string();
                 }
                 e => {
-                    eprintln!("Unknown field2: {}", e);
+                    return Err(format!("Unknown field2: {}", e).into());
                 }
             },
 
             _ => {
-                eprintln!("Content is missing");
+                return Err("Content is missing".into());
             }
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+struct AuthoredBy {
+    pen_name: String,
+    authors: Vec<AuthorName>,
+}
+
+impl UpdateField for AuthoredBy {
+    fn update_field(&mut self, tag: &Tag, doc: &Document) -> Result<()> {
+        match (tag.name.local_part.as_str(), doc) {
+            ("pen_name", Document::Content(Some(value))) => {
+                self.pen_name = value.to_string();
+                Ok(())
+            }
+            ("authors", Document::Nested(elements)) => {
+                elements.iter().try_for_each(
+                    |element| -> std::result::Result<(), Box<dyn std::error::Error>> {
+                        if let Document::Element(_, inner_doc, _) = element {
+                            let mut author_name = AuthorName::default();
+                            if let Document::Nested(inner_elements) = inner_doc.as_ref() {
+                                inner_elements.iter().try_for_each(
+                                    |inner_element| -> Result<()> {
+                                        if let Document::Element(tag, content, _) = inner_element {
+                                            author_name.update_field(tag, content)?;
+                                        }
+                                        Ok(())
+                                    },
+                                )?;
+                                self.authors.push(author_name);
+                            } else {
+                                return Err("Content is missing in Author authors".into());
+                            }
+                        }
+                        Ok(())
+                    },
+                )?;
+                Ok(())
+            }
+            _ => Err(format!("Unknown field in Author: {}", tag.name.local_part).into()),
         }
     }
 }
 
+#[derive(Debug, Default, Clone)]
+struct AuthorName {
+    first_name: String,
+    last_name: String,
+}
+impl UpdateField for AuthorName {
+    fn update_field(&mut self, tag: &Tag, doc: &Document) -> Result<()> {
+        let field_name = &tag.name.local_part;
+
+        if let Document::Content(Some(value)) = &doc {
+            match field_name.as_str() {
+                "first_name" => {
+                    self.first_name = value.to_string();
+                    Ok(())
+                }
+                "last_name" => {
+                    self.last_name = value.to_string();
+                    Ok(())
+                }
+                e => Err(format!("Unknown field in AuthorName: {}", e).into()),
+            }
+        } else {
+            Err("Content is missing in AuthorName".into())
+        }
+    }
+}
 fn main() -> Result<()> {
     let mut file = File::open("examples/TheExpanseSeries.xml")?;
     let data = read_file(&mut file)?;
     let (_, doc) = Document::parse_element_by_tag_name(&data, "book", &None)?;
     let mut book = Book::default();
+
     doc.iter_with_depth(0)
-        .filter_map(|record| {
-            if let Document::Element(tag, inner_doc, _) = record {
+        .filter_map(|element| {
+            if let Document::Element(tag, inner_doc, _) = element {
                 Some((tag, inner_doc))
             } else {
                 None
             }
         })
-        .for_each(|(tag, inner_doc)| book.update_field(tag, inner_doc));
+        .try_for_each(|(tag, inner_doc)| book.update_field(tag, inner_doc))
+        .map_err(|e| {
+            println!("Error updating field: {}", e);
+            e
+        })?;
+
     println!("{book:#?}");
     Ok(())
 }
