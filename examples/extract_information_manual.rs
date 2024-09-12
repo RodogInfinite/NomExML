4fab1b9cbe105b2227a403719475ab3a2355912e
RodogInfinite <wjroland@gmail.com>
2024-09-12 16:13:31 -0400

    Modify example to fit new style

--- examples/extract_information_manual.rs
+++ examples/extract_information_manual.rs
@@ -4,7 +4,7 @@ use nom_xml::{
     attribute::{Attribute, AttributeValue},
     io::read_file,
     tag::Tag,
-    update_fields, Document, Result, UpdateField,
+    Document, UpdateFields,
 };
 
 #[derive(Debug, Default)]
@@ -13,11 +13,16 @@ struct Book {
     authored_by: AuthoredBy,
     title: String,
     genre: String,
+    ty: String,
     series_number: u8,
     description: String,
 }
-impl UpdateField for Book {
-    fn update_field(&mut self, tag: &Tag, doc: &Document) -> Result<()> {
+impl UpdateFields for Book {
+    fn update_field(
+        &mut self,
+        tag: &Tag,
+        doc: &Document,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let field_name = &tag.name.local_part;
 
         if let Some(attributes_vec) = &tag.attributes {
@@ -35,26 +40,15 @@ impl UpdateField for Book {
         }
 
         match &doc {
-            Document::Nested(_) => {
-                for element in doc.iter_with_depth(1) {
-                    if let Document::Element(ref tag, ref inner_doc, _) = element {
-                        if "authored_by" == tag.name.local_part {
-                            update_fields(inner_doc, &mut self.authored_by)?;
-                        } else {
-                            self.update_field(tag, inner_doc)?;
-                        }
-                    } else {
-                        return Err(format!("Unknown field: {element:#?}").into());
-                    }
-                }
-            }
-
             Document::Content(Some(value)) => match field_name.as_str() {
                 "title" => {
                     self.title = value.to_string();
                 }
                 "genre" => {
                     self.genre = value.to_string();
+                }
+                "type" => {
+                    self.ty = value.to_string();
                 }
                 "series_number" => {
                     self.series_number = value.parse().unwrap_or_default();
@@ -66,6 +60,19 @@ impl UpdateField for Book {
                     return Err(format!("Unknown field2: {}", e).into());
                 }
             },
+            Document::Nested(_) => {
+                for element in doc.iter_with_depth(1) {
+                    if let Document::Element(tag, inner_doc, _) = element {
+                        if "authored_by" == tag.name.local_part {
+                            self.authored_by.update_fields(inner_doc)?;
+                        } else {
+                            self.update_field(tag, inner_doc)?;
+                        }
+                    } else {
+                        return Err(format!("Unknown field: {element:#?}").into());
+                    }
+                }
+            }
 
             _ => {
                 return Err("Content is missing".into());
@@ -82,8 +89,12 @@ struct AuthoredBy {
     authors: Vec<AuthorName>,
 }
 
-impl UpdateField for AuthoredBy {
-    fn update_field(&mut self, tag: &Tag, doc: &Document) -> Result<()> {
+impl UpdateFields for AuthoredBy {
+    fn update_field(
+        &mut self,
+        tag: &Tag,
+        doc: &Document,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         match (tag.name.local_part.as_str(), doc) {
             ("pen_name", Document::Content(Some(value))) => {
                 self.pen_name = value.to_string();
@@ -96,7 +107,7 @@ impl UpdateField for AuthoredBy {
                             let mut author_name = AuthorName::default();
                             if let Document::Nested(inner_elements) = inner_doc.as_ref() {
                                 inner_elements.iter().try_for_each(
-                                    |inner_element| -> Result<()> {
+                                    |inner_element| -> Result<(), Box<dyn std::error::Error>> {
                                         if let Document::Element(tag, content, _) = inner_element {
                                             author_name.update_field(tag, content)?;
                                         }
@@ -123,8 +134,12 @@ struct AuthorName {
     first_name: String,
     last_name: String,
 }
-impl UpdateField for AuthorName {
-    fn update_field(&mut self, tag: &Tag, doc: &Document) -> Result<()> {
+impl UpdateFields for AuthorName {
+    fn update_field(
+        &mut self,
+        tag: &Tag,
+        doc: &Document,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let field_name = &tag.name.local_part;
 
         if let Document::Content(Some(value)) = &doc {
@@ -144,7 +159,7 @@ impl UpdateField for AuthorName {
         }
     }
 }
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut file = File::open("examples/TheExpanseSeries.xml")?;
     let data = read_file(&mut file)?;
     let (_, doc) = Document::parse_element_by_tag_name(&data, "book", &None)?;
@@ -158,11 +173,7 @@ fn main() -> Result<()> {
                 None
             }
         })
-        .try_for_each(|(tag, inner_doc)| book.update_field(tag, inner_doc))
-        .map_err(|e| {
-            println!("Error updating field: {}", e);
-            e
-        })?;
+        .try_for_each(|(tag, inner_doc)| book.update_field(tag, inner_doc))?;
 
     println!("{book:#?}");
     Ok(())
